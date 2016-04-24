b490274ceba3ad02aaf21bc7d9e58d13a6e6259d
violinbeats <vincentscsu@gmail.com>
2016-04-23 20:30:35 -0700

    added simulation visualization

--- research/visualizations.R
+++ research/visualizations.R
@@ -73,3 +73,32 @@ write.csv(startVisits, 'yearlyCarto-start.csv', row.names = F)
 endVisits$visits = 1
 endVisits = data.table(endVisits)[, list(visits = sum(visits)), by='lat,lon']
 write.csv(endVisits, 'yearlyCarto-end.csv', row.names = F)
+
+# visualize simulation data
+avail1 = c(0.91, 0.91, 0.83 , 0.83, 0.91,0.91,0.83, 0.71,0.91,0.91, 0.83,0.91,0.91,0.83,0.83,0.83,0.83,0.83,0.91,0.91)
+profit1 = c(135, 135, 150, 150, 135, 135, 150,150,135,135,150, 135,135,150,150,150,150,150,135,135)
+
+avail2 = c(0.91, 0.91, 0.83, 0.83, 0.91,0.91,0.83,0.83,0.91,0.91,0.83,0.91,0.91,0.83,0.83,0.83,0.91,0.91,0.83,0.83)
+profit2 = c(135, 135, 150, 150, 135, 135, 150, 150, 135,135, 150,135,135,150,150,150,135,135,150,150)
+
+avail3 = c(0.83, 0.74, 0.71, 0.66, 0.71, 0.69, 0.71, 0.66, 0.69, 0.71, 0.69, 0.69, 0.69, 0.74, 0.69, 0.69, 0.77, 0.66,0.69,0.77)
+profit3 = c(40, 60, 80, 80, 60, 80, 80, 80, 60, 60, 80, 60, 80, 60, 80, 60, 60,80,60,60)
+
+# plot availability into one graph and profit into another
+df1 = as.data.frame(cbind(c(avail1, avail2, avail3)))
+names(df1) = "Value"
+df1$Objective = c(rep("Maximizing Availability",20), rep("Maximizing Profit", 20), rep("Minimizing Cost", 20))
+df1$X = as.factor(rep(1:20,3))
+ggplot(df1, aes(x=X, y=Value, col=Objective, group=Objective)) + 
+  geom_line() + geom_point() + 
+  labs(title = "Availability Comparison", x = "Weeks", y = "Weekly Avg. Availability") +
+  ylim(0.6,0.95)
+
+df2 = as.data.frame(cbind(c(profit1, profit2, profit3)))
+names(df2) = "Value"
+df2$Objective = c(rep("Maximizing Availability",20), rep("Maximizing Profit", 20), rep("Minimizing Cost", 20))
+df2$X = as.factor(rep(1:20,3))
+ggplot(df2, aes(x=X, y=Value, col=Objective, group=Objective)) + 
+  geom_line() + geom_point() + 
+  labs(title = "Profit Comparison", x = "Weeks", y = "Weekly Profit ($)") + 
+  ylim(0,150)
